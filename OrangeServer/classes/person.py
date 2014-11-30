7b85bc3a22f1e7063abc34d832663aada6c43d15
Shanu Salunke <shanu@gatech.edu>
2014-11-29 19:01:10 -0500

    Person class added

---
+++ OrangeServer/classes/person.py
@@ -0,0 +1,21 @@
+class Person:
+
+	def __init__():
+		#Display oriented
+		self.name = ""
+		self.motive = ""
+
+		#Story Oriented
+		self.inventory = []
+		self.personality = []
+		self.things_to_tell = []
+		self.mySOW = None
+		self.goals = []
+		self.current_location = None
+
+	def performAction(action):
+		print "Perform Action"
+
+	def doActionLoop():
+		print "Do Action Loop"
+
