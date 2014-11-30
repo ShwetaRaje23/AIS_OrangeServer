61ed381e8a11e56d34884f1a18e92a61b90a698e
ShwetaRaje <itsshwet@gmail.com>
2014-11-29 19:15:20 -0500

    added classes
    location / object

---
+++ OrangeServer/classes/location.py
@@ -0,0 +1,7 @@
+class Location:
+
+	def __init__():
+		self.name=""
+		self.objects_in_loc= []
+		self.char_in_loc=[]
+		self.adjacent=[]
---
+++ OrangeServer/classes/object.py
@@ -0,0 +1,13 @@
+class Object:
+	
+	def __init__():
+		self.name=""
+		self.is_weapon= False
+		self.is_used= False
+		self.used_name_append=""
+
+	def canPick(person_name):
+		#check if a person can pick a particular object or not. return bool. 
+
+
+
