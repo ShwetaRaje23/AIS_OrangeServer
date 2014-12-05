5c3376287f791d0cc11f765bb7199027c299de19
Shanu Salunke <shanu@gatech.edu>
2014-12-05 00:11:28 -0500

    csrf

--- OrangeServer/storyAgent.py
+++ OrangeServer/storyAgent.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from classes.game import Game
 
+
+# @csrf_exempt
+#     def dispatch(self, *args, **kwargs):
+#         return super(MyView, self).dispatch(*args, **kwargs)
+
 @csrf_exempt
 def playermessage(request,fromplayer,toplayer):
 	return ""
--- OrangeServer/urls.py
+++ OrangeServer/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import storyAgent
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'OrangeServer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^tellMeAStory/$', storyAgent.tellMeAStory),
+    url(r'^tellMeAStory/$', csrf_exempt(storyAgent.tellMeAStory)),
     url(r'^admin/', include(admin.site.urls)),
 )
