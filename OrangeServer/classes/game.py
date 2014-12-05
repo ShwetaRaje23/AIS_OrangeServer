383afa9cc71d41b9a1835197446bae5be30c22a4
Shanu Salunke <shanu@gatech.edu>
2014-12-05 05:17:27 -0500

    m

--- OrangeServer/classes/game.py
+++ OrangeServer/classes/game.py
@@ -22,6 +22,11 @@ class Game:
 	def tellMeAStory(self):
 		#this is where the input values will come into the code.
 
+		module_dir = os.path.dirname(__file__)  # get current directory
+		file_path = os.path.join(module_dir, "output.json")
+		return open(file_path)
+
+
 		jsonResponse = []
 		responseDict = {}
 
